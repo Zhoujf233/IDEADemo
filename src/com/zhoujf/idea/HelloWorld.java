c8dfc0d1a1ce5ada52d706f1dd8d0029e4749620
zhoujf <zhoujf1636@163.com>
2023-03-23 21:47:58 +0800

    hot-fix commit3

--- src/com/zhoujf/idea/HelloWorld.java
+++ src/com/zhoujf/idea/HelloWorld.java
@@ -37,7 +37,7 @@ public class HelloWorld {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello git!!!");
-        System.out.println("master test!!!");
+        System.out.println("hot-fix test!!!");
     }
 
 
