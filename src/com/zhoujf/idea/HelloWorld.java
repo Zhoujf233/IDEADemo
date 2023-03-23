c1bc8e08ae18f5315c4442e7268c1ae4f0e5f314
zhoujf <zhoujf1636@163.com>
2023-03-23 22:09:48 +0800

    push test1

--- src/com/zhoujf/idea/HelloWorld.java
+++ src/com/zhoujf/idea/HelloWorld.java
@@ -39,6 +39,7 @@ public class HelloWorld {
         System.out.println("hello git!!!");
         System.out.println("master test 2!!!");
         System.out.println("hot-fix test!!!");
+        System.out.println("push test!!!");
     }
 
 
